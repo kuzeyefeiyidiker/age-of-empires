23ea263a6dd8072814182c9bcd709f7ef069232c
kuzeyefeiyidiker <kuzeyiyidiker@gmail.com>
2023-04-07 22:26:39 +0300

    moved layout to parent directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-import { HeaderComponent } from './common/layout/header/header.component';
-import { LayoutComponent } from './common/layout/layout.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { NotFoundPageComponent } from './common/not-found-page/not-found-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LayoutComponent, NotFoundPageComponent],
-  imports: [BrowserModule, AppRoutingModule, StoreModule.forRoot({}, {})],
+  declarations: [AppComponent, LayoutComponent, HeaderComponent, NotFoundPageComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, StoreModule.forRoot({}, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnDestroy } from '@angular/core';
+import { NavigationCancel, NavigationEnd, NavigationError, NavigationStart, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.scss'],
+})
+export class LayoutComponent implements OnDestroy {
+  subscription: Subscription;
+  isNavigating = false;
+
+  constructor(private router: Router) {
+    this.subscription = this.router.events.subscribe(event => {
+      switch (true) {
+        case event instanceof NavigationStart: {
+          this.isNavigating = true;
+          break;
+        }
+        case event instanceof NavigationEnd:
+        case event instanceof NavigationCancel:
+        case event instanceof NavigationError: {
+          this.isNavigating = false;
+          break;
+        }
+        default: {
+          break;
+        }
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (!this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
